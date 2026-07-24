57bb5f5f1224866664e7047a8e6b1ab9c2471783
saibalajikothinti <saibalaji@panaroma.ai>
2026-07-25 00:53:22 +0530

    Add .gitignore and initial implementation of adjacency list representation for graphs

---
+++ graphs/adjacency_list_representation.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+
+class Graph:
+    def __init__(self, n, edges):
+        self.n = n
+        self.edges = edges
+
+    def build_adjacency_list(self):
+        adj = defaultdict(list)
+        
+        for u, v in self.edges:
+            adj[u].append(v)
+        return adj
+
+if __name__ == "__main__":
+    n = 4
+    edges = [[1, 0], [2, 0], [2,1], [3,1]]
+
+    practice_graph = Graph(n, edges)
+    print(practice_graph.build_adjacency_list())

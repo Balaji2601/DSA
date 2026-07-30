1b52fda2705e6fe1f0e16e553ea4eb774bd246d3
balaji2601 <balajikothinti26@gmail.com>
2026-07-30 13:48:30 +0530

    Add implementation for checking bipartite graphs using DFS

---
+++ graphs/is_bipartite_graph_dfs.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+
+
+class Solution:
+    def isBipartite(self, V, edges):
+
+        def check_bipartite_DFS(adj, u, color, currColor):
+            color[u] = currColor
+
+            for v in adj[u]:
+                if color[v] == color[u]: # If color of adj v node is same as u node then it is not a bipartite graph
+                    return False
+                if color[v] == -1: # If color[v] == -1 meaning v is not visited then we have to dfs on that
+                    color_of_v = 1 - currColor
+                    if check_bipartite_DFS(adj, v, color, color_of_v) == False:
+                        return False
+
+            return True
+
+        adj = defaultdict(list)
+
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+
+        # -1 in color indicates the node i is not visited and not coloured
+        color = [-1]*V
+
+        # red = 1
+        # grenn = 0
+
+        for i in range(V):
+            if color[i] == -1:
+                if check_bipartite_DFS(adj, i, color, 1) == False: # start the first node of a graph with red color(it is a choice)
+                    return False
+
+        return True

bffe4932f6ab4c2880443b9e4a5542342f0e5aa4
balaji2601 <balajikothinti26@gmail.com>
2026-08-02 20:02:05 +0530

    Add implementation of Dijkstra's algorithm for shortest path calculation

---
+++ graphs/dijkstra/dijkstra.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+import heapq as hq
+
+class Solution:
+    # Returns shortest distances from src to all other vertices
+    def dijkstra(self, V, edges, src):
+        adj = defaultdict(list)
+        for u, v, wt in edges:
+            adj[u].append((v, wt))
+            adj[v].append((u, wt))
+        
+        result = [float("inf")]*V # initializing distances from src to all vertices as inf(MAX_INT)
+        
+        result[src] = 0 # from src ---> src the shortest distance is zero right
+        pq = []
+        hq.heappush(pq, (0, src)) # pushing it to the min_heap
+        
+        while pq:
+            dist, node = hq.heappop(pq)
+             # check if the current node distance dist from the node is greater than in the result
+             # if it is greater than what in the result then we already found the distance to the node 
+             # from source, then going further does not make sense with dist. 
+             # So continue.
+            if dist > result[node]:
+                continue
+            for v,wt in adj[node]:
+                if dist+wt < result[v]:
+                    result[v] = dist+wt
+                    hq.heappush(pq, (dist+wt,v))
+        
+        return result

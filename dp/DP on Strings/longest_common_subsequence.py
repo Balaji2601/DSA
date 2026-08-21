2b94f61fab85a984d34ec4aaa7709ea2f2237c32
saibalajikothinti <saibalaji@panaroma.ai>
2026-08-21 16:06:01 +0530

    Add bottom-up implementation for longest common subsequence problem

--- dp/DP on Strings/longest_common_subsequence.py
+++ dp/DP on Strings/longest_common_subsequence.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/longest-common-subsequence/description/
 
+# recursion + memoization
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         n1 = len(text1)
@@ -27,3 +28,25 @@ class Solution:
             return max(choose, skip1, skip2)
 
         return solve(0,0)
+    
+
+# bottom up
+# state defination: dp[i][j] meaning LCS 
+# between 
+# S1 of length i 
+# S2 and of length j 
+class Solution2:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n1 = len(text1)
+        n2 = len(text2)
+
+        dp = [[0]*(n2+1) for _ in range(n1+1)]
+
+        for i in range(1,n1+1):
+            for j in range(1, n2+1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = 1+dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        
+        return dp[n1][n2]

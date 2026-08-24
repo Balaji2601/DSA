f4dfedc69c9e8a2520df80d1f2afa5bf37e79b05
Balaji2601 <balajikothinti26@gmail.com>
2026-08-25 01:26:50 +0530

    Add implementations for maximum number of points with cost and minimum falling path sum problems

---
+++ dp/DP on grids/maximum_number_points_with_cost.py
@@ -0,0 +1,53 @@
+# https://leetcode.com/problems/maximum-number-of-points-with-cost/description/
+
+from typing import List
+
+# O(m*n*n)
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        m = len(points)
+        n = len(points[0])
+        dp = [[0]*n for _ in range(m)]
+
+        for j in range(n):
+            dp[0][j] = points[0][j]
+        
+        for i in range(1,m):
+            for j in range(n):
+                val = -float("inf")
+                for k in range(n):
+                    val = max(points[i][j]+dp[i-1][k]-abs(j-k),val)
+                dp[i][j] = val
+        return max(dp[m-1])
+
+
+# O(m*n)
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        m = len(points)
+        n = len(points[0])
+
+        prev = [0]*n
+        for j in range(n):
+            prev[j] = points[0][j]
+        
+        
+        for i in range(1,m):
+            left = [0]*n
+            right = [0]*n
+            
+            left[0] = prev[0]
+            for k in range(1,n):
+                left[k] = max(prev[k], left[k-1]-1)
+            
+            right[n-1] = prev[n-1]
+            for k in range(n-2,-1,-1):
+                right[k] = max(prev[k], right[k+1]-1)
+            
+            curr = [0]*n
+            for k in range(n):
+                curr[k] = points[i][k] + max(left[k], right[k])
+            
+            prev = curr
+        
+        return max(prev)
